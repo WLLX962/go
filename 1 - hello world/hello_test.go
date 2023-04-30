f66e4074921e0042373094f66f40bbcf877fdde7
William K <wmkhgg@gmail.com>
2023-04-30 16:06:57 -0300

    refatorando testes

--- 1 - hello world/hello_test.go
+++ 1 - hello world/hello_test.go
@@ -3,17 +3,23 @@ package main
 import "testing"
 
 func TestSaudacao(t *testing.T) {
-	resultado := saudacao("")
-	experado := "Hello World"
-	if resultado != experado {
-		t.Errorf("resultado '%s', experado '%s'", resultado, experado)
-	}
-}
 
-func TestSaudacaoComNome(t *testing.T) {
-	resultado := saudacao("William")
-	experado := "Hello World, William"
-	if resultado != experado {
-		t.Errorf("resultado '%s', experado '%s'", resultado, experado)
+	validar := func(t *testing.T, resultado, experado string) {
+		t.Helper()
+		if resultado != experado {
+			t.Errorf("resultado '%s', experado '%s'", resultado, experado)
+		}
 	}
+
+	t.Run("Saudacao sem nome", func(t *testing.T) {
+		resultado := saudacao("")
+		experado := "Hello World 1"
+		validar(t, resultado, experado)
+	})
+
+	t.Run("Saudacao com nome", func(t *testing.T) {
+		resultado := saudacao("William")
+		experado := "Hello World, William 1"
+		validar(t, resultado, experado)
+	})
 }
